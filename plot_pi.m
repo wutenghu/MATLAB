ebe969d1e04204915b53582d8c3099b8d6df9b86
Tenghu Wu, PhD <wutenghu.ubc@gmail.com>
2016-01-31 01:19:49 -0800

    Update plot_pi.m

--- plot_pi.m
+++ plot_pi.m
@@ -9,14 +9,14 @@ TestNum=1e7;  % Produce 1e7 points
 X=2*(rand(TestNum,2)-0.5); 
 CircleNum=0;
 %
-% figure
-% hold on
+figure
+hold on
 for i=1:TestNum
     if X(i,1)^2+X(i,2)^2<=1
         CircleNum=CircleNum+1;
-%        plot(X(i,1),X(i,2),'r.')
+        plot(X(i,1),X(i,2),'r.')
     else
-%        plot(X(i,1),X(i,2),'bo')
+        plot(X(i,1),X(i,2),'bo')
     end
     
 end
